106529f7710e431348cfe7283c15b1cc3a88103f
jaimetrovoada <jaimetrovoada@gmail.com>
2022-08-19 18:24:15 +0800

    add websocket test

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
 </script>
+<script lang="ts">
+let res: unknown;
+export default {
+  data() {
+    return {
+      response: res,
+    };
+  },
+  methods: {
+    pingSocket() {
+      const url = "ws://localhost:8080/v1/api/ws";
+      const conn = new WebSocket(url);
+      const send = () => conn.send("hi");
+      conn.onmessage = (msg) => {
+        this.response = msg.data;
+        console.log(msg);
+      };
+      try {
+        conn.onopen = () => {
+          send();
+        };
+      } catch (err) {
+        console.log({ err });
+      }
+    },
+  },
+};
+</script>
 
 <template>
   <main>
     <TheWelcome />
     <RouterLink to="matchmaking">Find a match</RouterLink>
+    <button @click="pingSocket">ping</button>
+    <div>WebSocket message = {{ response }}</div>
   </main>
 </template>
--- server/cmd/server/main_test.go
+++ server/cmd/server/main_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/jaimetrovoada/hangman/internal/app"
 	"github.com/stretchr/testify/assert"
 )
@@ -20,4 +21,32 @@ func TestPingRoute(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestWebSocket(t *testing.T) {
+	r := gin.Default()
+	server := app.NewServer(r)
+
+	d := websocket.Dialer{}
+	s := httptest.NewServer(server.Routes().Handler())
+	defer s.Close()
+	_, resp, err := d.Dial("ws://"+s.Listener.Addr().String()+"/v1/api/ws", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, want := resp.StatusCode, http.StatusSwitchingProtocols
+
+	// msgT, _, err := c.ReadMessage()
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// msg := []byte(string("ping"))
+	// if err := c.WriteMessage(msgT, msg); err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	if ok := assert.Equal(t, want, got); !ok {
+		t.Errorf("want:\t%d\ngot:\t%d", want, got)
+	}
 }
